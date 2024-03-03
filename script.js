b821e650a2f59ea1d9a1bc6e48332e61969c8dd2
nazirul <nazirul.me01c@gmail.com>
2024-03-04 02:28:10 +0600

    search and loading functionality added

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const discussCardContainer = document.getElementById("discuss-card-container");
 const markasreadContainer = document.getElementById("markasread-container");
 const markAsReadNumber = document.getElementById("markAsReadNumber");
 const latestCardContainer = document.getElementById("latest-card-container");
+const errorMessage = document.getElementById("error-message");
+
+const searchInput = document.getElementById("searchinput");
+const searchBtn = document.getElementById("search-btn");
 
 let markAsReadCount = 0;
 
@@ -12,11 +16,21 @@ const getAllPostData = async () => {
   const responseJson = await response.json();
 
   const responseData = responseJson.posts;
-
+  discussCardContainer.innerHTML = "";
   showAllPost(responseData);
 };
 
-getAllPostData();
+function showLoading() {
+  discussCardContainer.innerHTML = `<div class='w-full flex gap-0'><img class="w-full" src="./images/Pulse-0.9s-204px.gif" alt="" /> <img class="w-full hidden lg:flex" src="./images/Pulse-0.9s-204px.gif" alt="" /></div>
+    
+  `;
+  setTimeout(() => {
+    discussCardContainer.innerHTML = "";
+    getAllPostData();
+  }, 2000);
+}
+
+showLoading();
 
 function showAllPost(data) {
   data.forEach((card) => {
@@ -29,12 +43,12 @@ function showAllPost(data) {
         card.image
       }" alt="" /></div>
       <span
-        class="block absolute right-1/3 lg:-top-1 lg:-right-1 w-4 h-4 rounded-full ${
+        class="block absolute -top-1 right-1/3 lg:-top-1 lg:-right-1 w-4 h-4 rounded-full ${
           card.isActive ? "bg-green-600" : "bg-red-600"
         }"
       ></span>
     </div>
-    <div class="flex flex-col gap-2">
+    <div class="space-y-2 w-full">
       <div class="flex justify-between text-[#12132DCC]">
         <p># ${card.category}</p>
         <p>Author : ${card.author.name}</p>
@@ -68,9 +82,9 @@ function showAllPost(data) {
           </p>
         </div>
         <div>
-          <button onclick="handleCardClick('${card.view_count}','${
+          <button onclick="handleCardClick('${card.view_count}','${escape(
       card.title
-    }')">
+    )}')">
             <img src="./images/icons/Group 40106.png" alt="" />
           </button>
         </div>
@@ -83,15 +97,16 @@ function showAllPost(data) {
 
 const handleCardClick = (views, title) => {
   markAsReadCount++;
+
   markAsReadNumber.innerText = `(${markAsReadCount})`;
   const div = document.createElement("div");
-  div.className = "bg-white p-2 flex justify-between gap-8 rounded-xl";
-  div.innerHTML = ` <h1 class="text-[#12132D] font-semibold max-w-44 lg:max-w-48">
-  ${title}
-</h1>
-<p class="flex items-center flex-1">
-  <img src="./images/icons/Group 16.png" alt="" /> ${views}
-</p>`;
+  div.className = "bg-white p-2 flex justify-between items-center rounded-xl";
+  div.innerHTML = ` <div><h1 class="text-[#12132D] font-semibold max-w-44 lg:max-w-56">
+  ${unescape(title)}
+</h1></div>
+<div><p class="flex items-center flex-1">
+<img src="./images/icons/Group 16.png" alt="" /> ${views}
+</p></div>`;
 
   markasreadContainer.appendChild(div);
 };
@@ -147,3 +162,29 @@ function showLatestPost(data) {
     latestCardContainer.appendChild(cardDiv);
   });
 }
+
+searchBtn.addEventListener("click", async () => {
+  const searchInputValue = searchInput.value;
+  if (searchInputValue !== "") {
+    errorMessage.innerText = "";
+    const response = await fetch(
+      `https://openapi.programming-hero.com/api/retro-forum/posts?category=${searchInputValue}`
+    );
+    const responseJson = await response.json();
+    const data = responseJson.posts;
+
+    //show loading
+    discussCardContainer.innerHTML = `<div class='w-full flex gap-0'><img class="w-full" src="./images/Pulse-0.9s-204px.gif" alt="" /> <img class="w-full hidden lg:flex" src="./images/Pulse-0.9s-204px.gif" alt="" /></div>
+    `;
+    setTimeout(() => {
+      discussCardContainer.innerHTML = "";
+      if (data.length === 0) {
+        discussCardContainer.innerHTML = `<div class="lg:w-96"><h1 class='text-3xl font-black text-[#7B7DFCFF] w-full mr-5'>Oops! Not Found...</h1></div>`;
+        return;
+      }
+      showAllPost(data);
+    }, 2000);
+  } else {
+    errorMessage.innerText = "Input Field Cannot Be Empty !";
+  }
+});
